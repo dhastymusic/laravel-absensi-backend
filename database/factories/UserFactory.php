5869030a0b28d004d47b219c93a88984fa046cbf
dhastymusic <dhastymusic@gmail.com>
2024-04-22 13:01:28 +0700

    auth

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,13 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
@@ -24,11 +22,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'phone' => $this->faker->phoneNumber(), // Include definition for the 'phone' column
+            'role' => 'user', // Include definition for the 'role' column
         ];
     }
 
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.dashboard');
+    return view('pages.auth.auth-login');
+});
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function(){
+        return view('pages.dashboard');})->name('home');
 });
